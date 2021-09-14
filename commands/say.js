22fb2a36e1dbaf9bfebdce5fda5c0774d1009042
1rens1 <rein.satyawarman@gmail.com>
2021-09-14 10:03:08 +0700

    Delete author message

--- commands/say.js
+++ commands/say.js
@@ -4,5 +4,6 @@ module.exports = {
     execute(message, args) {
         const sayMessage = message.content.split(" ").slice(1).join(" ");
         message.channel.send(sayMessage);
+        message.delete();
     }
 }
