0c817d2b18803521764dddd81de4eb959383dc72
1rens1 <rein.satyawarman@gmail.com>
2021-09-22 11:17:25 +0700

    Change Author to Footer

--- commands/say.js
+++ commands/say.js
@@ -6,7 +6,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
             .setColor("RANDOM")
             .setDescription(message.content.split(" ").slice(1).join(" "))
-            .setAuthor(message.author.tag, message.author.avatarURL())
+            .setFooter(message.author.tag, message.author.avatarURL())
             .setTimestamp();
         message.channel.send(embed);
     },
