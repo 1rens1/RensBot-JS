cab15508e9f5a2334265186823def08f30d83205
1rens1 <rein.satyawarman@gmail.com>
2021-09-22 11:19:36 +0700

    Removed Embed Color

--- commands/say.js
+++ commands/say.js
@@ -4,7 +4,6 @@ module.exports = {
     execute(message, args, Discord) {
         message.delete();
         const embed = new Discord.MessageEmbed()
-            .setColor("RANDOM")
             .setDescription(message.content.split(" ").slice(1).join(" "))
             .setFooter(message.author.tag, message.author.avatarURL())
             .setTimestamp();
