c0dd8e2e21556975541edaf6eedb2dfd062d4f11
1rens1 <rein.satyawarman@gmail.com>
2021-09-22 11:23:46 +0700

    Changed Author to Footer

--- commands/say.js
+++ commands/say.js
@@ -5,7 +5,7 @@ module.exports = {
         message.delete();
         const embed = new Discord.MessageEmbed()
             .setDescription(message.content.split(" ").slice(1).join(" "))
-            .setFooter(message.author.tag, message.author.avatarURL())
+            .setAuthor(message.author.tag, message.author.avatarURL())
             .setTimestamp();
         message.channel.send(embed);
     },
